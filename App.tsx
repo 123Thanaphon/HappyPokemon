cbbeff0ce23325942505d039e12d00a3037c01f0
Thanaphon Senarak <thanaphon.s@noblehome.com>
2025-12-04 13:43:23 +0700

    Test add text with style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Pokemon Yeah!!</Text>
 			<StatusBar style="auto" />
+			<Text style={styles.textTitle}>Pokemon Yeah!!</Text>
 		</View>
 	);
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-container: {
-	flex: 1,
-	backgroundColor: '#fff',
-	alignItems: 'center',
-	justifyContent: 'center',
-},
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	textTitle: {
+		fontSize: 30,
+		fontWeight: 'bold',
+	}
 });
